7ad5c349be22875bdcb92dd38c40bbbc2f7b7d9f
Ian Moran <ian.moran11@gmail.com>
2021-09-12 21:30:49 +1000

    more documentation

--- R/evaluate.R
+++ R/evaluate.R
@@ -3,14 +3,40 @@
 #'
 #' @param g ralget
 #' @export
- 
+#' @examples
+#' \dontrun{
+#'
+#'  product <- function(...){list(...) %>% reduce(`*`)}
+#'  const <- function(x){function(...){x}}
+#'  
+#'  S01 <-   v("S01",   .func = const(10000))
+#'  I01 <-   v("I01",   .func = const(10))
+#'  BIS01 <- v("BIS01", .func = product)
+#'  
+#'  S02 <-   v("S02",   .func = sum)
+#'  I02 <-   v("I02",   .func = sum)
+#'  
+#'  SIR <- 
+#'  S01    * ( e(.func = function(.value){.value * .5} ) * BIS01  + 
+#'             e(.func = function(.value){.value * 1} )  * S02 ) +
+#'  
+#'  I01    * ( e(.func = function(.value){.value * 1} ) * BIS01  +
+#'             e(.func = function(.value){.value * 1}) * I02 )  +
+#'  
+#'  BIS01  *  (e(.func = function(.value){.value * 1} ) * S02  +
+#'             e(.func = function(.value){.value * -2}) *  I02 ) 
+#'  
+#'  evaluate(SIR)
+#' }
+
+
 evaluate <- function(g){
 g %>% 
 mutate(order = node_topo_order())  %>%
  arrange(order) %>% 
  mutate(edge_funcs = map(row_number(), ~ .E()[.E() %>% pull(to) == .x,] %>% pull(.attrs) )) %>% 
  mutate(edge_args = map(row_number(), ~ .E()[.E() %>% pull(to) == .x,] %>% pull(from_name) )) %>%
- mutate(eval_statement = pmap_dbl(  
+ mutate(eval_statement = pmap(  
     list(name = name,.attrs = .attrs, edge_funcs= edge_funcs,edge_args= edge_args), 
      .f = function(...){
         wk <- list(...)
