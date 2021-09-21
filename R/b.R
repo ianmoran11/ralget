89313c78b3ddf6d2c316f700e0ab072995c8166a
Ian Moran <ian.moran11@gmail.com>
2021-09-21 21:41:26 +1000

    raldag stuff removed- package building

--- R/b.R
+++
@@ -1,9 +0,0 @@
-
-#' A shorthand to represent coefficient in a data generating process.
-#'
-#' @param m coefficient value 
-#' @export
- 
-b <- function(m){ 
-    e(.func = function(.value){.value * m})
-    }
--- R/d.R
+++
@@ -1,6 +0,0 @@
-#' Specify a data generating process
-#'
-#' @param ... a data generating process as an anonymous equation
-#' @export
-
-d <- rlang::as_function
