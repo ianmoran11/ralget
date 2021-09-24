505ec3401f491de67df9be4b628b0469685bfbd7
Ian Moran <ian.moran11@gmail.com>
2021-09-25 00:00:35 +1000

    coverage of evaluate functions

--- tests/testthat/test-evaluation.R
+++ tests/testthat/test-evaluation.R
@@ -1,14 +1,14 @@
 test_that("multiplication works", {
 
-a <- v("a", .f = rlang::as_function((~ 1)))
-b <- v("b", .f = rlang::as_function((~ 2)))
-c <- v("c", .f = rlang::as_function((~ sum(.x))))
+a <- v("a", .f = rlang::as_function(~ 1))
+b <- v("b", .f = rlang::as_function(~ 2))
+c <- v("c", .f = rlang::as_function(~ rsum(.x)))
 
-d <- (a + b)*c
+e1 <- e(.func = function(.value){.value})
 
-d1 <- d %>% activate("edges") %>% mutate(.attrs = map(1,rlang::as_function( ~ .x))) %>% activate("nodes") %>% get_edge_names() %>%  activate("nodes")
+d <- (a*e1 + b*e1)*c
 
-d1 %>% evaluate_prepare() 
+result <- d %>% evaluate_prepare() %>% evaluate_execute() %>% pull(eval_statement) %>% unlist()
 
-expect_true(T)
+expect_true(all(result == c(1,2,1)))
 })
