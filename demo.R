1aae9e0ee3d126f8591d3f1f722365708b774f19
Ian Moran <ian.moran11@gmail.com>
2021-09-25 20:09:40 +1000

    tidying up

--- demo.R
+++ demo.R
@@ -1,32 +1,14 @@
 library(devtools)
 document()
 install()
-usethis::use_testthat()
-usethis::use_test(name = "commutative-addition")
-usethis::use_test(name = "evaluation")
-usethis::use_test(name = "distributive-multiplication")
-usethis::use_test(name = "distributive-multiplication-with-edges")
-usethis::use_test(name = "ralget-sum")
-usethis::use_pkgdown()
-install.packages("DT")
-usethis::use_coverage()
-?usethis::use_covr_ignore
-covr::codecov()
-#rm(print.ralget)
-plotter <- function(graph){
-usethis::use_github_actions()
-?covr::report
-covr::report()
-?usethis::use_coverage
-usethis::use_github_action("test-coverage")
 
+
+# Test ----------------------------------------------------
 circle <- v("a") * v("b") * v("c")
 line <- v("1")*v("2")*v("3")
 
 as_tibble(circle %x% line) %>% pull(name)
 as_tibble(activate(circle %x% line,"edges")) 
-
-#graph <- (v("a")+ v("b"))
 
 l <- tribble(
     ~name,~x,~y,
@@ -36,39 +18,6 @@ l <- tribble(
    "d", 2,1)
  
 layout_df <- as_tibble(graph) %>% left_join(l) %>% select(x,y)
-
-if(nrow( as_tibble(activate(graph,"edges"))) == 0){
-p <- 
-  graph %>% 
-      ggraph(layout_df)  +
-      geom_node_point(col = "grey66", size = 20, show.legend = FALSE) +
-      geom_node_text(aes(label = name), size =15) +
-      theme_graph() + xlim(.5,2.5) + ylim(.5,2.5)
-
-return(p)
-}
-
-  graph %>% 
-      ggraph(layout_df)  +
-      geom_edge_link(
-        aes(end_cap =   circle(25, "pt"),
-            start_cap = circle(25, "pt")),
-        edge_colour = "black",
-        arrow = arrow(
-          angle = 15,
-          length = unit(0.15, "inches"),
-          ends = "last",
-          type = "closed"
-        ),
-        width = 1.2
-      ) +
-      geom_node_point(col = "grey66", size = 20, show.legend = FALSE) +
-      geom_node_text(aes(label = name), size = 15) +
-      theme_graph() + xlim(.5,2.5) + ylim(.5,2.5)
-}
-
-v("a") + v("b") * (v("c") + v("d")) 
-
 
 a <- v("a")
 b <- v("b")
