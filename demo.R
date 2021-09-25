e620e6a4bb24c1f688c5933164caa18be8a8a526
Ian <ian.moran11@gmail.com>
2021-09-25 08:32:31 +0000

    fixing evaluate

--- demo.R
+++ demo.R
@@ -1,111 +1,53 @@
 library(devtools)
-document()
-install()
-usethis::use_testthat()
-usethis::use_test(name = "commutative-addition")
-usethis::use_test(name = "distributive-multiplication")
-usethis::use_pkgdown()
+rm(list = ls())
+devtools::document()
+devtools::install()
+library(tidyverse)
+library(tidygraph)
+library(ggraph)
+library(patchwork)
+library(magrittr)
+library(ralget)
+library(ggforce)
+library(raldag)
+library("conflicted")
+options(dplyr.print_max = 3,dplyr.print_min = 3)
+conflict_prefer("do", "raldag")
+conflict_prefer("filter", "dplyr")
+conflict_prefer("simulate", "raldag")
 
-usethis::use_coverage()
-
-
-#rm(print.ralget)
-plotter <- function(graph){
-
-#graph <- (v("a")+ v("b"))
-
-l <- tribble(
-    ~name,~x,~y,
-   "a", 1,2,
-   "b", 2,2,
-   "c", 1,1,
-   "d", 2,1)
- 
-layout_df <- as_tibble(graph) %>% left_join(l) %>% select(x,y)
-
-if(nrow( as_tibble(activate(graph,"edges"))) == 0){
-p <- 
-  graph %>% 
-      ggraph(layout_df)  +
-      geom_node_point(col = "grey66", size = 20, show.legend = FALSE) +
-      geom_node_text(aes(label = name), size =15) +
-      theme_graph() + xlim(.5,2.5) + ylim(.5,2.5)
-
-return(p)
-}
-
-  graph %>% 
-      ggraph(layout_df)  +
-      geom_edge_link(
-        aes(end_cap =   circle(25, "pt"),
-            start_cap = circle(25, "pt")),
-        edge_colour = "black",
-        arrow = arrow(
-          angle = 15,
-          length = unit(0.15, "inches"),
-          ends = "last",
-          type = "closed"
-        ),
-        width = 1.2
-      ) +
-      geom_node_point(col = "grey66", size = 20, show.legend = FALSE) +
-      geom_node_text(aes(label = name), size = 15) +
-      theme_graph() + xlim(.5,2.5) + ylim(.5,2.5)
-}
-
-v("a") + v("b") * (v("c") + v("d")) 
+c <- v("c", .f = d(~ rnorm(n = 10^4, mean = rsum(.x) + 10, sd =  4)))
+a <- v("a", .f = d(~ rnorm(n = 10^4, mean = rsum(.x)     , sd =  1)))
+x <- v("x", .f = d(~ rnorm(n = 10^4, mean = rsum(.x) + 30, sd =  4)))
+y <- v("y", .f = d(~ rnorm(n = 10^4, mean = rsum(.x) +  7, sd =  2)))
+m <- v("m", .f = d(~ rnorm(n = 10^4, mean = rsum(.x) + 40, sd =  2)))
 
 
-a <- v("a")
-b <- v("b")
-c <- v("c")
-d <- v("d")
+g <-
+  (a * b(9) + m * b(1)) * x +
+  (c * b(2) + m * b(5)) * y +
+  (a * b(3) + c * b(5)) * m
 
-a + b * (c + d)
+
+g %>% evaluate_prepare() %>% evaluate_execute()
+
+
+g %>% evaluate_prepare() %>% evaluate_execute()
+
+
+
+h <-
+  (m * b(2) + x * b(1)) * y
+
+
+
+h %>% evaluate_prepare() %>% evaluate_execute()
 
 
 
 
+q <- (m * b(2) + x * b(1)) * y  +  ( y * b(2)*c)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-g <- (v("a") + v("b")) * (v("c") + v("d")))
-plotter(g)
-g
-plotter(v("a"))
-plotter(v("a") + v("b"))
-
-
-print.ralget <- function(g){
-   print(plotter(g))
-   class(g) <- class(g)[-1] 
-   print(g)
-    }
-print.ralget <- function(g){g}
-rm(print.ralget)
-plotter(g,l)
+q %>% evaluate_prepare() %>% filter(row_number() == 1) %>% evaluate_execute()
+q %>% evaluate_prepare() %>% filter(row_number() <= 2) %>% evaluate_execute()
+q %>% evaluate_prepare() %>% evaluate_execute()
