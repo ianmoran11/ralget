597646d448830390820893d4bdf2cc327f118c18
Ian <ian.moran11@gmail.com>
2021-09-30 09:55:22 +0000

    first commit

--- demo.R
+++ demo.R
@@ -5,6 +5,10 @@ document()
 install()
 test()
 check()
+usethis::use_pkgdown()
+library(usethis)
+
+usethis::use_pkgdown_github_pages()
 
 library(tidyverse)
 library(tidygraph)
@@ -22,7 +26,7 @@ circle <- v("a") * v("b") * v("c")
 line <- v("1")*v("2")*v("3")
 
 as_tibble(circle %x% line) %>% pull(name)
-as_tibble(activate(circle %x% line,"edges")) 
+as_tibble(activate(circle %x% line,"edges"))
 
 a <- v("a")
 b <- v("b")
@@ -47,7 +51,7 @@ q %>% evaluate_prepare() %>% filter(row_number() <= 2) %>% evaluate_execute()
 q %>% evaluate_prepare() %>% evaluate_execute()
 
 
-# Raldag 
+# Raldag
 
 c <- v("c", .f = d(~ rnorm(n = 10^4, mean = rsum(.x) + 10, sd =  4)))
 a <- v("a", .f = d(~ rnorm(n = 10^4, mean = rsum(.x)     , sd =  1)))
@@ -62,30 +66,30 @@ t3 <- v("t3", .f = d(~ rnorm(n = 10^4, mean = rsum(.x), sd =  1)))
 t4 <- v("t4", .f = d(~ rnorm(n = 10^4, mean = rsum(.x), sd =  1)))
 t5 <- v("t5", .f = d(~ rnorm(n = 10^4, mean = rsum(.x), sd =  1)))
 
-t <- 
+t <-
 (t1 * b(1) * t2) +
 (t2 * b(1) * t3) +
 (t3 * b(1) * t4) +
-(t4 * b(1) * t5) 
+(t4 * b(1) * t5)
 
 g <-
  (a * b(9) + m * b(1)) * x +
  (c * b(2) + m * b(5)) * y +
  (a * b(3) + c * b(5)) * m
 
-xt <- cartesian_product(g,t,node_combine =  ~ c(.x,.y), edge_combine =  ~ c(.x,.y)) 
+xt <- cartesian_product(g,t,node_combine =  ~ c(.x,.y), edge_combine =  ~ c(.x,.y))
 
-xt %>% activate("edges") %>% mutate(daggity_text= paste0(from_name, " -> ",to_name)) %>% 
+xt %>% activate("edges") %>% mutate(daggity_text= paste0(from_name, " -> ",to_name)) %>%
 pull(daggity_text) %>% paste(collapse = "\n") %>% paste("dag {",.,"}") %>% dagitty::dagitty() %>%
 tidy_dagitty() %>%
 ggdag(.) +
   theme_dag()
 
 
-xtc <- 
-xt %>% 
-    select(-.attrs.y) %>% 
-    activate("edges") %>% 
+xtc <-
+xt %>%
+    select(-.attrs.y) %>%
+    activate("edges") %>%
     mutate(.attrs = map2(.attrs,.attrs.y, ~ c(.x,.y))) %>%
     select(-.attrs.y) %>%
     activate("nodes")  %>%
